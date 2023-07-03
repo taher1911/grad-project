a1742212e49a9c3b55579cf48f9020f2d56dfc68
taher1911 <72612843+taher1911@users.noreply.github.com>
2023-07-03 12:28:18 -0400

    Add files via upload

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -27,13 +27,13 @@ export const socials = [
 ];
 const Footer = () => (
   <footer className={`w-full py-14 relative mt-16 `}>
-    <div className="footer-gradient" />
+    <div className="footer-gradient w-[223px] md:w-[400px] h-[112px] md:h-[70px]" />
     <div className={` mx-auto flex flex-col gap-8 w-[90%] max-w-[1300px]`}>
       <div className="flex flex-col">
         <div className="flex flex-col md:flex-row items-center justify-between flex-wrap gap-4 text-center md:text-left">
           <Link
             href={"/"}
-            className="font-extrabold text-[24px] text-white tracking-wider uppercase"
+            className="font-extrabold text-xl md:text-[24px] text-white tracking-wider uppercase"
           >
             Detectiva
           </Link>
